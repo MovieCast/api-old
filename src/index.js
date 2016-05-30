c549ca1cbe3a428eb35adede218a9fcefa677b2d
Paul Hobbel <paulhobbel@hotmail.com>
2016-05-30 02:10:15 +0200

    That is a typo

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class Main {
 
     loadCronTasks() {
         // Run scraper every 4 hours.
-        this.createCronTask('0 0 */4 * * *', this.scraper.scape());
+        this.createCronTask('0 0 */4 * * *', this.scraper.run());
     }
 
     createCronTask(cronTime, cronFn) {
