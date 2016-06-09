7433f44243c46be9c42aeb15697f421626cbeadb
Paul Hobbel <paulhobbel@hotmail.com>
2016-06-09 14:56:52 +0200

    This fixes #6

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ class Main {
         this.logger = new logger();
         this.scraper = new scraper(config);
         this.api = new api(config.server);
-
         this.loadStorage();
         this.loadCronTasks();
 
@@ -29,18 +28,19 @@ class Main {
 
     loadCronTasks() {
         // Run scraper every 4 hours.
-        this.createCronTask('0 0 */4 * * *', this.scraper.run());
+        this.createCronTask('0 0 */4 * * *', this.scraper.run.bind(this.scraper));
     }
 
     createCronTask(cronTime, cronFn) {
         const job = new CronJob({
             cronTime: cronTime,
             onTick: () => {
-                cronFn;
+                cronFn();
             },
+            start: true,
             timeZone: 'America/Los_Angeles'
         });
-        job.start();
+        cronFn();
     }
 
     run() {
